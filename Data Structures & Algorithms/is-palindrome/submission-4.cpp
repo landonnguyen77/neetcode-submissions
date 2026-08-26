b9c7a20a2f2e4691ebc14766165a2df1526b5e02
landonnguyen77 <121177231+landonnguyen77@users.noreply.github.com>
2026-08-26 13:14:32 -0500

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r)
+        {
+            while (l < r && !isAlph(s[l]))
+            {
+                l++;
+            }
+            while (r > l && !isAlph(s[r]))
+            {
+                r--;
+            }
+            if (tolower(s[l]) != tolower(s[r]))
+            {
+                return false;
+            } 
+            l++;
+            r--;
+        }
+        return true;
+    }
+    bool isAlph(char c)
+    {
+        return (c >= 'A' && c <= 'Z' ||
+        c >= 'a' && c <= 'z' ||
+        c >= '0' && c <= '9');
+    }
+};
